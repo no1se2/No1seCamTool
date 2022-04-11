c5161533b48ab330f700a82fb70953e1f50c059e
NO1SE <98566890+no1se2@users.noreply.github.com>
2022-04-10 23:23:19 -0400

    Delete No1se-Cam-Tool directory

--- No1se-Cam-Tool/jio/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-include 'ip.php';
-header('Location: https://catalogs-advanced-dead-ones.trycloudflare.com/index2.html');
-exit
-?>
--- No1se-Cam-Tool/om/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-include 'ip.php';
-header('Location: https://community-vacuum-exam-workout.trycloudflare.com/index2.html');
-exit
-?>
